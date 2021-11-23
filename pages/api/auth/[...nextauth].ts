24166a68587892ad8df28ff7aa882b93e240b039
Dane Miller <khadane.miller@gmail.com>
2021-11-22 19:23:07 -0500

    add email magic link login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,23 @@
 import NextAuth from "next-auth/next";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import EmailProvider from "next-auth/providers/email";
 import { prisma } from "lib/db";
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   // TODO: configure log in providers
-  providers: [],
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  secret: process.env.SECRET,
+  session: {
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  jwt: {
+    secret: "ASFGKBDGWERQ@#E#!@@R$Q2Q@#!@!@#$",
+  },
+  debug: true,
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
 import { useQuery } from "@apollo/client";
+import { useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
+  const { data: session, status } = useSession();
+
+  console.log(session, status);
+
   return <div>hello friend</div>;
 };
 
