c90b21f58439dd3db4f0ad7cec3f21611197925e
Dane Miller <khadane.miller@gmail.com>
2021-12-12 17:16:11 -0500

    login modal

--- components/Elements/Auth/Auth.tsx
+++ components/Elements/Auth/Auth.tsx
@@ -7,8 +7,14 @@ import {
   ModalBody,
   ModalCloseButton,
   Button,
-  useDisclosure,
+  FormControl,
+  FormLabel,
+  Input,
+  Divider,
+  Box,
+  Text,
 } from "@chakra-ui/react";
+import { AiOutlineGoogle } from "react-icons/ai";
 
 type Props = {
   isOpen: boolean;
@@ -20,14 +26,34 @@ export const Auth = ({ isOpen, onClose }: Props) => {
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Create your account</ModalHeader>
+        <ModalHeader>Log in to critter</ModalHeader>
         <ModalCloseButton />
-        <ModalBody pb={6}>aaaaa</ModalBody>
-        <ModalFooter>
-          <Button colorScheme="blue" mr={3}>
-            Login
+        <ModalBody pb={6}>
+          <form>
+            <FormControl mb="3">
+              <FormLabel>Email</FormLabel>
+              <Input placeholder="user@example.com" type="email" />
+            </FormControl>
+            <Button colorScheme="blue" mr={3} w="100%">
+              Send magic link to e-mail
+            </Button>
+          </form>
+          <Box display="flex" alignItems="center" my={4}>
+            <Divider />
+            <Text px="4" color="gray.500">
+              or
+            </Text>
+            <Divider />
+          </Box>
+          <Button
+            colorScheme="red"
+            w="100%"
+            aria-label="Log in with your google account"
+            leftIcon={<AiOutlineGoogle />}
+          >
+            Log in with Google
           </Button>
-        </ModalFooter>
+        </ModalBody>
       </ModalContent>
     </Modal>
   );
