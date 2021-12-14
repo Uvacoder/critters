407854048a8436b217b5249b1541235b92664d32
Dane Miller <khadane.miller@gmail.com>
2021-12-13 19:52:04 -0500

    more footer updates

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,25 +1,28 @@
-import { Box, Divider, Icon, Flex, Link } from "@chakra-ui/react";
+import { Box, Divider, Icon, Flex, Link, Text } from "@chakra-ui/react";
 import { FaGithub, FaTwitterSquare } from "react-icons/fa";
 
 export const Footer = () => {
   return (
     <Box as="footer" maxWidth="8xl" margin="auto" py="4rem" px="1rem">
       <Divider mb="2rem" />
-      <Flex color="gray.500" justifyContent="flex-end" gridGap="3">
-        <Link
-          isExternal
-          href="https://github.com/nulfrost/critters"
-          aria-label="View this project on github, opens in a new tab"
-        >
-          <Icon as={FaGithub} h={8} w={8} />
-        </Link>
-        <Link
-          isExternal
-          href="https://twitter.com/hybridearth"
-          aria-label="Visit Dane's profile on twitter, opens in a new tab"
-        >
-          <Icon as={FaTwitterSquare} h={8} w={8} />
-        </Link>
+      <Flex justifyContent="space-between" alignItems="center">
+        <Text color="gray.500">Made in Toronto by Dane.</Text>
+        <Flex color="gray.500" justifyContent="flex-end" gridGap="3">
+          <Link
+            isExternal
+            href="https://github.com/nulfrost/critters"
+            aria-label="View this project on github, opens in a new tab"
+          >
+            <Icon as={FaGithub} h={8} w={8} />
+          </Link>
+          <Link
+            isExternal
+            href="https://twitter.com/hybridearth"
+            aria-label="Visit Dane's profile on twitter, opens in a new tab"
+          >
+            <Icon as={FaTwitterSquare} h={8} w={8} />
+          </Link>
+        </Flex>
       </Flex>
     </Box>
   );
