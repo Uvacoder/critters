1d88feb6a3749f38b3995a27b7bca2dddeca647a
Dane Miller <khadane.miller@gmail.com>
2021-12-15 22:25:29 -0500

    add basic account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,7 +1,25 @@
-import { Heading, Flex } from "@chakra-ui/react";
+import {
+  Heading,
+  Input,
+  FormControl,
+  FormLabel,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { MainLayout } from "components/Layout";
+import { useSession } from "next-auth/react";
 
 const Account = () => {
+  const { data: session } = useSession();
+  const { onClose, onOpen, isOpen } = useDisclosure();
+
   return (
     <MainLayout title="account">
       <Heading
@@ -12,6 +30,42 @@ const Account = () => {
       >
         Account
       </Heading>
+      <FormControl id="email">
+        <FormLabel>Email</FormLabel>
+        <Input
+          isDisabled
+          type="email"
+          defaultValue={session?.user?.email}
+          colorScheme="blue"
+          name="email"
+          variant="filled"
+          maxW="md"
+          mb="4"
+        />
+      </FormControl>
+      <Button colorScheme="red" onClick={onOpen} variant="ghost">
+        Delete Account
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            Are you sure you want to delete your account?
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Deleting your account will also remove any active posts you have.
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost" colorScheme="red" onClick={onClose}>
+              Yes, delete my account
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </MainLayout>
   );
 };
